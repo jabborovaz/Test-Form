cffb4c33b090424d5ab4c1e2d574baeee5007135
jabborovaz <greatman-98@mail.ru>
2023-11-18 23:41:10 +0500

    setup app

---
+++ src/components/InputBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InputBox() {
+  return <div>InputBox</div>;
+}
+
+export default InputBox;
---
+++ src/container/Main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../App.module.scss";
+
+function Main() {
+  return <div className={styles.container}>Main</div>;
+}
+
+export default Main;
